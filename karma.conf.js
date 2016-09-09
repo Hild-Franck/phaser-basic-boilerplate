767e7cf711c334d565a44ea20d2425baf4e9907b
Franck <franck@imac-de-claude-pradel.local>
2016-09-09 18:19:12 +0200

    Removed useless script lines

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
       { pattern: 'src/**/*.js', watched: true },
       { pattern: 'test/*.js', watched: true }
     ],
